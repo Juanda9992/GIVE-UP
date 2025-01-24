b2dcabe9a040fb8f7e66b30925812cd9d58b289b
Juanda999 <juanda999games@gmail.com>
2025-01-24 01:49:43 -0500

    Add logic for creating scriptables for the levels

---
+++ Assets/Scripts/Level/Level_Compiler.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Level_Compiler : Editor
+{
+
+    [MenuItem("Tools/Compile Level")]
+    public static void CompileLevel()
+    {
+        Object_Editor_Data[] objectsToStore = GameObject.FindObjectsOfType<Object_Editor_Data>();
+
+        Level_Data newLevel = ScriptableObject.CreateInstance<Level_Data>();
+        
+        string pathToCreateLevel = "Assets/Scriptables/Level/";
+
+        string[] existingScriptables = Directory.GetFiles(pathToCreateLevel,"*.asset");
+
+        string fileName = "Level_" + (existingScriptables.Length +1)+".asset";
+        AssetDatabase.CreateAsset(newLevel,Path.Combine(pathToCreateLevel,fileName));
+        Debug.Log($"Found {objectsToStore.Length} objects in scene" );
+    }
+}
---
+++ Assets/Scripts/Level/Object_Editor_Data.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Object_Editor_Data : MonoBehaviour
+{
+    public ObjectType objectType;
+    public Vector2 objectPosition;
+}
