b995f17edfceccd7cdb5f618e1c285e112a11023
Juanda999 <juanda999games@gmail.com>
2025-01-24 22:30:11 -0500

    Add logic for opening levels in editor

--- Assets/Scripts/Level/Level_Compiler.cs
+++ Assets/Scripts/Level/Level_Compiler.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Level_Compiler : Editor
@@ -25,5 +26,23 @@ public class Level_Compiler : Editor
         string fileName = "Level_" + (existingScriptables.Length +1)+".asset";
         AssetDatabase.CreateAsset(newLevel,Path.Combine(pathToCreateLevel,fileName));
         Debug.Log($"Found {objectsToStore.Length} objects in scene" );
+    }
+
+    [OnOpenAsset]
+    public static bool LoadLevelOnEditor(int instanceId, int line)
+    {
+        Level_Data levelLoaded = EditorUtility.InstanceIDToObject(instanceId) as Level_Data;
+
+        if(levelLoaded != null)
+        {
+            GameObject.FindObjectOfType<Level_Handler>().LoadLevel(levelLoaded);
+        }
+
+        return false;
+    }
+    [MenuItem("Tools/Reset Level")]
+    private static void ClearLevelInEditor()
+    {
+        GameObject.FindObjectOfType<Level_Handler>().ClearLevelInEditor(); 
     }
 }
